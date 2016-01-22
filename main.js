32b364f249351e90b69e1814b7a4df1c7c797e7c
roschart <jlbalirac@hotmail.com>
2016-01-22 15:12:47 +0100

    Delete all and start for the basics

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.getElementById('content').innerHTML = 'test lifeReload'
+(() => {
+  'use strict'
+  document.getElementById('content').innerHTML = 'test lifeReload'
+  var data = [4, 8, 15, 16, 23, 42]
+  d3.select('#chart')
+    .selectAll('div')
+    .data(data)
+    .enter().append('div')
+    .style('width', function (d) { return d * 10 + 'px' })
+    .text(function (d) { return d })
+})()
