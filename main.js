a2bac440c6282f919ab534ac8aeb923ceb38fc63
roschart <jlbalirac@hotmail.com>
2016-01-25 14:40:15 +0100

    Use d3 from npm and add range example

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 (() => {
   'use strict'
-  document.getElementById('content').innerHTML = 'test lifeReload'
-  var data = [4, 8, 15, 16, 23, 42]
-  d3.select('#chart')
+  var d3 = require('d3')
+  const size = {width: 420}
+  const data = [4, 8, 15, 16, 23, 42]
+
+  d3.select('#simple')
     .selectAll('div')
     .data(data)
     .enter().append('div')
     .style('width', function (d) { return d * 10 + 'px' })
+    .text(function (d) { return d })
+
+  const x = d3.scale.linear().domain([0, d3.max(data)]).range([0, size.width])
+  d3.select('#scale')
+    .selectAll('div')
+    .data(data)
+    .enter().append('div')
+    .style('width', function (d) { return x(d) + 'px' })
     .text(function (d) { return d })
 })()
